162cfc384fba98dfb4787ad4b4ce9474122a7953
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2020-11-08 05:21:09 +0200

    Feat: setting up trip controller in progress

---
+++ app/Http/Controllers/API/TripController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Trip;
+use Illuminate\Http\Request;
+
+class TripController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $trips = Trip::all();
+        return $this->sendResponse($trips,'trips');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+        'name' => ['required', 'max:255'],
+        ]);
+
+        $trip = Trip::create([
+            'name' => $request->get('name'),
+        ]);
+
+        return $this->sendResponse($trip,'trip created sussfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trip $trip)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trip $trip)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trip $trip)
+    {
+        //
+    }
+}
