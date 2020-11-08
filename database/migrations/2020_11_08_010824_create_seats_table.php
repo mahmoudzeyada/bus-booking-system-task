11cb90078716fc263ba305f04310c067818bed69
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2020-11-08 05:20:09 +0200

    Feat:creating migrations files for db

--- database/migrations/2020_11_08_010824_create_seats_table.php
+++ database/migrations/2020_11_08_010824_create_seats_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSeatsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('seats', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['USER', 'ADMIN']);
+            $table->foreignId('start_station');
+            $table->foreignId('end_station');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('seats');
     }
 }
