b70c9c7a904ddfa27fc12cb5da197a23c7546245
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2020-11-10 00:39:58 +0200

    Feat: add logic to show function in controller

--- app/Http/Controllers/API/TripController.php
+++ app/Http/Controllers/API/TripController.php
@@ -5,20 +5,11 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Trip;
 use Illuminate\Http\Request;
-
+use App\Models\TripStation;
+use App\Models\Seat;
+use App\Models\Station;
 class TripController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-        $trips = Trip::all();
-        return $this->sendResponse($trips,'trips');
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -39,37 +30,61 @@ class TripController extends Controller
         return $this->sendResponse($trip,'trip created sussfully');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Trip  $trip
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Trip $trip)
+
+    public function show($name)
     {
-        //
+        // validaing
+        $trip = Trip::where('name',$name)->first();
+        if (!$trip) {
+            return $this->sendError('trip not found', []);
+        }
+        $trip->avalibale_seats = $this->number_of_avaliable_seats_for_each_station_in_trip($trip);
+        return $this->sendResponse($trip, '');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Trip  $trip
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Trip $trip)
-    {
-        //
+
+    private function number_of_avaliable_seats_for_each_station_in_trip($trip) {
+        /**
+         * we sort it by rank desc because we sort stations in trip by rank(ex):
+         * 1-cairo 0 -> start_of_line
+         * 2-mansora 1
+         * 3-dametta 2
+         * 4-bortsaid 3 -> end_of_line
+         * so here if we want to compute number of avaliable seats in mansora we should
+         * count the number of seats in dametta and bortsaid as (start or end station) and the number of start station in mansora
+         * and substart that with
+         * the original number of seat of the trip
+         * */
+        $tripStations = TripStation::where('trip_id', $trip->id)->orderby('rank', 'desc')->get();
+        $number_of_seats_in_stations = [];
+        forEach($tripStations as $tripStation) {
+            $trip_stations_has_greater_rank_ids = $this->get_greater_ranks_ids($tripStations, $tripStation->rank);
+            $trip_stations_has_greater_or_equal_rank_ids = $trip_stations_has_greater_rank_ids;
+            array_push($trip_stations_has_greater_or_equal_rank_ids, $tripStation->id);
+            $number_of_seats = Seat::whereIn('start_station', $trip_stations_has_greater_or_equal_rank_ids)
+                                    ->orWhereIn('end_station', $trip_stations_has_greater_rank_ids)
+                                    ->count();
+            $remaining_seats = $trip->numberOfSeats - $number_of_seats;
+            $remaining_seats = $remaining_seats > 0 ? $remaining_seats : 0;
+
+            $station = Station::where('id', $tripStation->station_id)->get()->first();
+
+            array_push($number_of_seats_in_stations,(object)['remaining_seats' => $remaining_seats, 'station' => $station, 'rank' => $tripStation->rank]);
+        }
+        return array_reverse($number_of_seats_in_stations);
+     }
+
+
+
+    private function get_greater_ranks_ids($array, $excluded_rank) {
+        $new_array = [];
+        foreach($array as $item) {
+            if ($item->rank > $excluded_rank) {
+                array_push($new_array, $item->id);
+            }
+        }
+        return $new_array;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Trip  $trip
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Trip $trip)
-    {
-        //
-    }
+
 }
